c06ac10b38282d3c4a62d2c1f63f0404fb68189b
Swee Sen <sweesenworkstation@gmail.com>
2022-02-24 16:27:16 +0000

    add keep alive for replit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from replit_keep_alive import keep_alive
 from time import sleep
 import slot_database
 import telebot
 import user_slot_tracker
 
-
+keep_alive()
 slot_database.startUpdatingDatabase()
 sleep(6)
 user_slot_tracker.startChecking()
